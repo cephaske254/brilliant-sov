81c9123bc5197ab6ff4ff7a19e1b996941524432
cephaske254 <cephaske254@gmail.com>
2022-07-24 16:29:17 +0300

    created theme context

---
+++ contexts/ThemeContext.tsx
@@ -0,0 +1,20 @@
+import colorAlpha from "color-alpha";
+import { createContext, ReactNode } from "react";
+import theme from "../theme/theme";
+
+const initialValues = {
+  ...theme,
+  alpha: colorAlpha,
+};
+const ThemeContext = createContext(initialValues);
+
+const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <ThemeContext.Provider value={initialValues}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export { ThemeContext, ThemeProvider };
+
